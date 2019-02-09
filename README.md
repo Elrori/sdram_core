ab180e13b199cbe04e756236fc858736369dfdc7
Helrori <helrori2011@gmail.com>
2019-02-09 13:20:34 +0800

    add init_done

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
-# sdram_core
 ```
   Name         :sdram_core.v
-  Description  :a simple SDRAM controller in page burst mode;固定的初始化后，用户设置起始
-                bank、行、列地址，进行固定长度连续单页读写，到达规定个数(wr_num)，该模块将
-                给出信号。注意：该模块仅读取单页(一行，读写字数可控,需要读取另一行，必更
-                换地址)。该模块将SDRAM当成显存来用(为了配合VGA)。不支持随机读写。该模块不
-                含FIFO。CAS==3;clk leads the 80 degree of sdram_clk(Different results
+  Description  :a simple SDRAM controller in full page burst mode,该模块意指在使用最简单的
+                状态转换机的前提下完成CMOS->VGA的图像缓存任务，SDRAM设置为简单的全页读写模
+                式，由于中间不能被refresh打断，为保证最短刷新时间，读写前后都有 失选和刷新。
+                读写个数越大效率越高，但不能超过列数; 模块流程 : 固定的初始化后，用户设置起
+                始bank、行、列地址，发送请求进行固定长度连续单页读写(请求必然导致读写操作！
+                即发一次读写请求后用户必须依照xx_allow信号去读写一次)，到达规定个数(wr_num)，
+                该模块将xx_busy置低，读写请求可以同时发生。
+                注意：sdram_core.v读写完全独立user依据wr_xxx端口信号进行控制无需知道rd_xxx
+                      的信号，同样，依据rd_xxx端口信号进行控制无需知道wr_xxx的信号。
+                注意：xx_request有效时间应当小于8个clk
+                注意：读一次后必须等rd_busy置低再进行下一次读操作，写也是一样。
+                注意：该模块仅读取单页(一行，读写字数可控,需要读取另一行，必更换地址)。该模
+                      块将SDRAM当成显存来用。不支持随机读写(wr_num==1时可以实现随机读写，但
+                      是效率较低)。该模块不含FIFO。
+                CAS==3;clk leads the 80 degree of sdram_clk(Different results
                 for different boards).Recommended sdram_clk phase shift -80 degrees.
                 Note:xx_request effective time should less than 8 clk,and do not Repeat 
-                xx_request until xx_allow finished.wr_data no pre-fetch need!
+                xx_request until xx_busy deasserted(wr_xxx rd_xxx independent).
+                wr_data no pre-fetch need!
   Origin       :190119
                 190122
+                190124 - add init_done
   Author       :helrori2011@gmail.com
-  Reference    :github.com/stffrdhrn/sdram-controller
+  Reference    :
+
 ```
-write 4 words
-![write 4 words](https://github.com/Elrori/sdram_core/blob/master/wr.png)
-read 4 words
-![read 4 words](https://github.com/Elrori/sdram_core/blob/master/rd.png)
-same time
-![rd_wr_in_same_time](https://github.com/Elrori/sdram_core/blob/master/rd_wr_in_same_time.png)
-read to write
-![read to write](https://github.com/Elrori/sdram_core/blob/master/rd2wr.png)
--- sdram_core.v
+++ sdram_core.v
@@ -2,16 +2,23 @@
 *  Name         :sdram_core.v
 *  Description  :a simple SDRAM controller in full page burst mode,该模块意指在使用最简单的
 *                状态转换机的前提下完成CMOS->VGA的图像缓存任务，SDRAM设置为简单的全页读写模
-*                式，由于中间不能被refresh打断，为保证最短刷新时间，读写前后都有 失选和刷
-*                新。读写个数越大效率越高，但不能超过列数; 流程 : 固定的初始化后，用户设置起
+*                式，由于中间不能被refresh打断，为保证最短刷新时间，读写前后都有 失选和刷新。
+*                读写个数越大效率越高，但不能超过列数; 模块流程 : 固定的初始化后，用户设置起
 *                始bank、行、列地址，发送请求进行固定长度连续单页读写(请求必然导致读写操作！
-*                即发一次写请求后用户必须依照wr_allow信号去写一次...)，到达规定个数(wr_num)，
-*                该模块将给出信号。注意：该模块仅读取单页(一行，读写字数可控,需要读取另一行，
-*                必更换地址)。该模块将SDRAM当成显存来用。不支持随机读写。该模块
-*                不含FIFO。CAS==3;clk leads the 80 degree of sdram_clk(Different results
+*                即发一次读写请求后用户必须依照xx_allow信号去读写一次)，到达规定个数(wr_num)，
+*                该模块将xx_busy置低，读写请求可以同时发生。
+*                注意：sdram_core.v读写完全独立user依据wr_xxx端口信号进行控制无需知道rd_xxx
+*                      的信号，同样，依据rd_xxx端口信号进行控制无需知道wr_xxx的信号。
+*                注意：xx_request有效时间应当小于8个clk
+*                注意：读一次后必须等rd_busy置低再进行下一次读操作，写也是一样。
+*                注意：该模块仅读取单页(一行，读写字数可控,需要读取另一行，必更换地址)。该模
+*                      块将SDRAM当成显存来用。不支持随机读写(wr_num==1时可以实现随机读写，但
+*                      是效率较低)。该模块不含FIFO。
+*                CAS==3;clk leads the 80 degree of sdram_clk(Different results
 *                for different boards).Recommended sdram_clk phase shift -80 degrees.
 *                Note:xx_request effective time should less than 8 clk,and do not Repeat 
-*                xx_request until xx_allow finished.wr_data no pre-fetch need!
+*                xx_request until xx_busy deasserted(wr_xxx rd_xxx independent).
+*                wr_data no pre-fetch need!
 *  Origin       :190119
 *                190122
 *                190124 - add init_done
@@ -289,9 +296,9 @@ always@(posedge clk or negedge rst_n)begin
                 sdram_cmd   <=  CMD_NOP;
             end
             //make rd_allow
-            if(word_cnt == 3)//***caution!!***这里的取值实际与仿真有出入，按照理论这里的取值应等于CAS，实际在板调试取值等于CAS即可,仿真MT的模型取CAS-1
+            if(word_cnt == 3)//***caution!!***
                 rd_allow <= 1'd1;
-            else if(word_cnt == 3 + rd_num_b)//***caution!!***这里的取值实际与仿真有出入，按照理论这里的取值应等于CAS+rd_num_b,仿真MT的模型取CAS-1+rd_num_b
+            else if(word_cnt == 3 + rd_num_b)//***caution!!***
                 rd_allow <= 1'd0;
             else
                 rd_allow <= rd_allow;
